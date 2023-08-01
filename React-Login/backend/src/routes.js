aea0e09a7db372b0115d0c32e5cccc623b2f23a9
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-08-01 14:27:28 -0300

    att login.js, gitignore, routes.js, server.js

--- React-Login/backend/src/routes.js
+++ React-Login/backend/src/routes.js
@@ -1,21 +1,23 @@
 const express = require('express');
 
 const routes = express.Router();
-const user = [{
+const users = [{
     id: 1,
     name: 'Juan',
     email: 'contato@bitloop.com.br',
     password: '123456'
-}]
+}];
 
 routes.post('/login', (req, res) => {
     const { email, password } = req.body;
 
     const user = users.find(user => user.email === email && user.password === password);
-    if (user) {
+    if (user)
+    {
         return res.status(200).json(user);
     }
-    return res.status(401).json({ message: 'Invalid credentials' });
+
+    return res.status(401).json({ message: 'Credenciais inválidas' });
 });
 
 module.exports = routes;
