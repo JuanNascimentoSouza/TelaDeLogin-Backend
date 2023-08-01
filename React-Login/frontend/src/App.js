9c7b097ef836f8b23da27218a5b378829b4ede81
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-08-01 14:29:08 -0300

    att, app.js, index.js

--- React-Login/frontend/src/App.js
+++ React-Login/frontend/src/App.js
@@ -1,17 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="app-header">
-        <div className='login-form-wrap'>
-          <h2>Login</h2>
-          <form className='login-form'>
-            <input type='email' name='email' placeholder='email' required></input>
-            <input type='password' name='password' placeholder='password' required></input>
-            <button type='submit' className='btn-login'>Login Screen</button>
-          </form>
-        </div>
+      <header className="App-header">
+      <Login />
       </header>
     </div>
   );
